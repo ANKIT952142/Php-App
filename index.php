3ef03ef5a6bf4788f7a0ac6e1599fff237f928a5
Ankit <ankit@example.com>
2023-07-13 10:47:24 +0000

    third commit

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php
-echo "Hello, World!";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Website</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>This is a sample PHP website.</p>
+</body>
+</html>
 
